efb70932d973807f288b5c8e2ef6a16ddbf6d579
Cindi Nishimoto <cindi_nishi@hotmail.com>
2023-12-21 18:50:40 -0300

    refactor: add isMediumSize

--- lib/widgets/upduo_team_widget.dart
+++ lib/widgets/upduo_team_widget.dart
@@ -6,6 +6,8 @@ class UpduoTeamWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var isMobile = MediaQuery.sizeOf(context).width < 800;
+    var isMediumSize = MediaQuery.sizeOf(context).width > 800 &&
+        MediaQuery.sizeOf(context).width < 2050;
 
     const assetsImagePath = "assets/images/me_learning_rust_and_ts.png";
     const text =
@@ -24,33 +26,63 @@ class UpduoTeamWidget extends StatelessWidget {
               ],
             ),
           )
-        : Padding(
-            padding: const EdgeInsets.only(
-              left: 200,
-              top: 100,
-              right: 200,
-              bottom: 100,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Text(
-                  text,
-                  style: TextStyle(
-                    fontSize: 43,
-                    fontWeight: FontWeight.w600,
-                  ),
+        : isMediumSize
+            ? Padding(
+                padding: const EdgeInsets.only(
+                  left: 100,
+                  top: 100,
+                  right: 100,
+                  bottom: 100,
                 ),
-                Expanded(
-                  child: Image.asset(
-                    height: 700,
-                    width: double.maxFinite,
-                    assetsImagePath,
-                    fit: BoxFit.cover,
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      text,
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Image.asset(
+                        height: 700,
+                        width: double.maxFinite,
+                        assetsImagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
+              )
+            : Padding(
+                padding: const EdgeInsets.only(
+                  left: 200,
+                  top: 100,
+                  right: 200,
+                  bottom: 100,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      text,
+                      style: TextStyle(
+                        fontSize: 43,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Expanded(
+                      child: Image.asset(
+                        height: 700,
+                        width: double.maxFinite,
+                        assetsImagePath,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 }
